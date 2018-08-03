8118ecd46ea25a45826c0f0f5997bedee029faeb
Pranav Sanvatsarkar <pranav.sanvatsarkar@enzigma.com>
2018-08-04 00:58:49 +0530

    - added two modules - request and body-parser. request is used to send HTTP request to remote server and body-parser is used to parse the request in JSON format so that we can refer the headers and body parts - we also implemented the Razor kind of notation in EJS that allows us to inject the JavaScript code in HTML which binds to the data sent by the server

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const apiKey = '44be76a34740228b2c54bec0a6cebe57';
+const request = require('request');
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('static_resources'));
 
 app.get('/', function (req, res) {
     //res.send('Hello World!');
     res.render('index');
+});
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let weatherText = `It's 25 degrees ${city}!`;
+    
+    res.render('index', { weather: weatherText, error: null, name: city });
 });
 
 app.listen(3000, function () {
